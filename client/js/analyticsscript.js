cb2616a6cacbc3fdb3c88ed4bd4876f573f6bd99
Bas Volkers TU <W.S.volkers@student.tudelft.nl>
2017-12-21 22:01:28 +0100

    ae

--- client/js/analyticsscript.js
+++ client/js/analyticsscript.js
@@ -2,14 +2,14 @@ var main = function(){
 	var getGoodHabitPercToday = function(){
 		$.getJSON("/../getGoodHabitPercToday.json",function(percentageJSON){
 			console.log(percentageJSON['percentage']);
-			$(".first p").text(percentageJSON['percentage'] + "%");
+			$(".first p").text((Math.round(parseFloat(percentageJSON['percentage']) * 100) / 100) + "%");
 			
 		});
 	}
 	var getBadHabitPercToday = function(){
 		$.getJSON("/../getBadHabitPercToday.json",function(percentageJSON){
 			console.log(percentageJSON['percentage']);
-			$(".second p").text(percentageJSON['percentage'] + "%");
+			$(".second p").text((Math.round(parseFloat(percentageJSON['percentage']) * 100) / 100) + "%");
 		});
 	}
 
